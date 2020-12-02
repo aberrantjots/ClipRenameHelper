1cdfd0b29020370f1c53fc1358a3a6727c92a926
aberrantjots <40366866+aberrantjots@users.noreply.github.com>
2020-12-02 12:02:25 -0700

    Initial commit

---
+++ Project1/MainForm.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace ClipHelper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.browseOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.counter = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.helpButton = new System.Windows.Forms.Button();
+            this.testCaseTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.counter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File name of clip:";
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(113, 40);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(297, 20);
+            this.fileNameTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Day: ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(113, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(113, 66);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 4;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // browseOpenFileDialog
+            // 
+            this.browseOpenFileDialog.DefaultExt = "mp4";
+            this.browseOpenFileDialog.Filter = "MP4 files|*.mp4|All file|*.*";
+            this.browseOpenFileDialog.InitialDirectory = "Downloads";
+            this.browseOpenFileDialog.Title = "Select Clip File";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "File name result:";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(113, 178);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(297, 20);
+            this.resultTextBox.TabIndex = 6;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(63, 110);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(107, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Outdoor_Pick-Up";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(187, 111);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(109, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Outdoor_Drop-Off";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(63, 142);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(99, 17);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Indoor_Pick-Up";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Click += new System.EventHandler(this.radioButton3_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(187, 142);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(101, 17);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Indoor_Drop-Off";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Click += new System.EventHandler(this.radioButton4_Click);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(317, 110);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(41, 17);
+            this.radioButton5.TabIndex = 11;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Pet";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.Click += new System.EventHandler(this.radioButton5_Click);
+            // 
+            // counter
+            // 
+            this.counter.Location = new System.Drawing.Point(367, 142);
+            this.counter.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.counter.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.counter.Name = "counter";
+            this.counter.Size = new System.Drawing.Size(44, 20);
+            this.counter.TabIndex = 12;
+            this.counter.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.counter.ValueChanged += new System.EventHandler(this.counter_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(314, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Counter:";
+            // 
+            // helpButton
+            // 
+            this.helpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.helpButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.helpButton.Location = new System.Drawing.Point(336, 223);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(75, 23);
+            this.helpButton.TabIndex = 14;
+            this.helpButton.Text = "Help!";
+            this.helpButton.UseVisualStyleBackColor = true;
+            this.helpButton.Click += new System.EventHandler(this.helpButton_Click);
+            // 
+            // testCaseTextBox
+            // 
+            this.testCaseTextBox.Location = new System.Drawing.Point(310, 12);
+            this.testCaseTextBox.Name = "testCaseTextBox";
+            this.testCaseTextBox.Size = new System.Drawing.Size(100, 20);
+            this.testCaseTextBox.TabIndex = 15;
+            this.testCaseTextBox.Text = "4784605";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(236, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Test Case #: ";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 270);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.testCaseTextBox);
+            this.Controls.Add(this.helpButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.counter);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Clip Rename Helper";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.counter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.OpenFileDialog browseOpenFileDialog;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.NumericUpDown counter;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button helpButton;
+        private System.Windows.Forms.TextBox testCaseTextBox;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Project1/MainForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ClipHelper
+{
+    public partial class MainForm : Form
+    {
+        private string _ogExtension = string.Empty;
+        private string _ogPath = string.Empty;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            var result = browseOpenFileDialog.ShowDialog(this);
+            if (result != DialogResult.OK) return;
+            fileNameTextBox.Text = browseOpenFileDialog.FileName;
+            resultTextBox.Text = browseOpenFileDialog.SafeFileName;
+            _ogExtension = browseOpenFileDialog.DefaultExt;
+            _ogPath = browseOpenFileDialog.FileName.Replace(browseOpenFileDialog.SafeFileName, string.Empty);
+        }
+
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            radioButton2.Checked = radioButton3.Checked = radioButton4.Checked = radioButton5.Checked = !radioButton1.Checked;
+            UpdateResult();
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = radioButton3.Checked = radioButton4.Checked = radioButton5.Checked = !radioButton2.Checked;
+            UpdateResult();
+        }
+
+        private void radioButton3_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = radioButton2.Checked = radioButton4.Checked = radioButton5.Checked = !radioButton3.Checked;
+            UpdateResult();
+        }
+
+        private void radioButton4_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = radioButton2.Checked = radioButton3.Checked = radioButton5.Checked = !radioButton4.Checked;
+            UpdateResult();
+        }
+
+        private void radioButton5_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = radioButton2.Checked = radioButton3.Checked = radioButton4.Checked = !radioButton5.Checked;
+            UpdateResult();
+        }
+        private void counter_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateResult();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateResult();
+        }
+
+        private void UpdateResult()
+        {
+            foreach (var control in Controls)
+            {
+                if (control is RadioButton radioButton && radioButton.Checked)
+                {
+                    resultTextBox.Text = $"{testCaseTextBox.Text}_Day{numericUpDown1.Value}_{radioButton.Text}_{counter.Value}.{_ogExtension}";
+                }
+            }
+        }
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            var fileCheck = File.Exists(fileNameTextBox.Text);
+            if (!fileCheck)
+            {
+                MessageBox.Show("Error: Bad filename?");
+                return;
+            }
+
+            var destination = $"{_ogPath}{resultTextBox.Text}";
+            var destinationCheck = File.Exists(destination);
+            if (!destinationCheck)
+            {
+                MessageBox.Show("Error: that file already exists. Update your selections.");
+            }
+            // Rename the file
+            try
+            {
+                File.Move(fileNameTextBox.Text, destination);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Error: {exception.Message}");
+            }
+
+        }
+    }
+}
